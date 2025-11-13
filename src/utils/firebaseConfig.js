7dabb5ca8cb965e0ae5d3746e6a525908bd565d8
MarcusC <marcusgchan@gmail.com>
2025-11-12 22:16:21 -0800

    no env

--- src/utils/firebaseConfig.js
+++ src/utils/firebaseConfig.js
@@ -15,14 +15,24 @@ import {
 
 console.log("Loaded API KEY:", FIREBASE_API_KEY);
 
+// const firebaseConfig = {
+//   apiKey: FIREBASE_API_KEY,
+//   authDomain: FIREBASE_AUTH_DOMAIN,
+//   projectId: FIREBASE_PROJECT_ID,
+//   storageBucket: FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
+//   appId: FIREBASE_APP_ID,
+//   measurementId: FIREBASE_MEASUREMENT_ID,
+// };
+
 const firebaseConfig = {
-  apiKey: FIREBASE_API_KEY,
-  authDomain: FIREBASE_AUTH_DOMAIN,
-  projectId: FIREBASE_PROJECT_ID,
-  storageBucket: FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID,
-  appId: FIREBASE_APP_ID,
-  measurementId: FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyC6KZ_yZGrJRfiY4qpaQqqYFW4x6915De4",
+  authDomain: "palanner-c8edc.firebaseapp.com",
+  projectId: "palanner-c8edc",
+  storageBucket: "palanner-c8edc.firebasestorage.app",
+  messagingSenderId: "375339816481",
+  appId: "1:375339816481:web:f5f9f63c38ea603e563560",
+  measurementId: "G-WPM56YT3W9"
 };
 
 export const firebase_app = initializeApp(firebaseConfig);
